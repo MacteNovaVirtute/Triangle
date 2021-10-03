509ccf02402d6d00b00da19ff697638bfc0e120f
Name <mactenovavirtute@protonmail.com>
2021-10-03 15:28:22 +0300

    Triangels

---
+++ src/com/company/Triangles.java
@@ -0,0 +1,49 @@
+package com.company;
+
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class Triangles {
+
+    public static void main(String[] args) {
+        for(int i = 0; i<3; i++){
+            for(int j = 0; j<3; j++){
+                if(i>=j){
+                    System.out.print("*");
+                }
+
+            }
+            System.out.println();
+
+        }
+        System.out.println("");
+        for(int i = 0; i<3; i++){
+            for(int j = 0; j<3; j++){
+                if(i<=j){
+                    System.out.print("*");
+                } else if(i>j){
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+
+        }
+        System.out.println("");
+
+        for(int j=1;j <= 3;j++){
+            for(int i=1;i <= 3-j;i++){
+                System.out.print(" ");
+            }
+
+            for(int k=1;k<=j; k++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
